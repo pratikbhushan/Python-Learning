9e5bb032abf96f550fc66ecba968a25314ab7327
pratikbhushan <pratikbhushan100@gmail.com>
2025-10-22 23:39:42 +0530

    add high score feature

--- Day20-21/Snake Game/main.py
+++ Day20-21/Snake Game/main.py
@@ -34,14 +34,14 @@ while is_game_on:
         
     #detect collision with the wall:
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        is_game_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     #detect collision with the tail:
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            is_game_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
         
 
