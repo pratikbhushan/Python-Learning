1f3c45aa0a02be57bae531b47ed33533bcc7e04b
pratikbhushan <pratikbhushan100@gmail.com>
2025-10-22 23:42:16 +0530

    add reset method

--- Day20-21/Snake Game/scoreboard.py
+++ Day20-21/Snake Game/scoreboard.py
@@ -7,20 +7,27 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open(r"C:\Users\vishw\OneDrive\Desktop\Python learning\Projects\Day20-21\Snake Game\data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 260)
         self.hideturtle()
+        self.update_score()
 
     def update_score(self):
-        self.write(f"Score: {self.score}", font=FONT, align=ALIGNMENT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", font=FONT, align=ALIGNMENT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", font=FONT, align=ALIGNMENT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open(r"C:\Users\vishw\OneDrive\Desktop\Python learning\Projects\Day20-21\Snake Game\data.txt", mode = "w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_score()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_score()
         
