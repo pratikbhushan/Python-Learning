20792db387a4584f75b5c10a3c406f11d896b846
pratikbhushan <pratikbhushan100@gmail.com>
2025-10-23 18:38:39 +0530

    starting_letter

---
+++ Day24/Mail Merge Project/main.py
@@ -0,0 +1,12 @@
+PLACEHOLDER = "[name]"
+
+with open(r"C:\Users\vishw\OneDrive\Desktop\Python learning\Projects\Day24\Mail Merge Project Start\Input\Names\invited_names.txt") as names_file:
+    names = names_file.readlines()
+   
+with open(r"C:\Users\vishw\OneDrive\Desktop\Python learning\Projects\Day24\Mail Merge Project Start\Input\Letters\starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"C:/Users/vishw/OneDrive/Desktop/Python learning/Projects/Day24/Mail Merge Project Start/Output/ReadyToSend/letter_for_{stripped_name}.docx", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
