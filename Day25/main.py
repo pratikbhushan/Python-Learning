877dbe672c8ee32e25614b907e780bb17d3fcf47
pratikbhushan <pratikbhushan100@gmail.com>
2025-10-24 22:27:14 +0530

    add main

---
+++ Day25/main.py
@@ -0,0 +1,27 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = r"C:\Users\vishw\OneDrive\Desktop\Python learning\Projects\Day25\blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv(r"C:\Users\vishw\OneDrive\Desktop\Python learning\Projects\Day25\50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50", prompt="What's another state's name?").title()
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
+
+
+screen.exitonclick()
