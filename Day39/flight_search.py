37f8c2e39c81e3019aa58680f82502d47dd78bcb
pratikbhushan <pratikbhushan100@gmail.com>
2026-04-28 00:23:47 +0530

    Add flight search

---
+++ Day39/flight_search.py
@@ -0,0 +1,35 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class FlightSearch:
+    
+    def __init__(self):
+        self.url = "https://serpapi.com/search"
+        self._api_key = os.environ.get("SERP_API_KEY")
+        
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        self.param = {"engine" : "google_flights",
+                    "departure_id": origin_city_code,
+                    "arrival_id": destination_city_code,
+                    "outbound_date": from_time.strftime("%Y-%m-%d"),
+                    "return_date": to_time.strftime("%Y-%m-%d"),
+                    "type": "1",
+                    "adults": "1",
+                    "currency": "INR",
+                    "api_key": self._api_key,}
+        response = requests.get(url=self.url, params=self.param)
+        
+        if response.status_code != 200:
+            print(f"check_flights() response code: {response.status_code}")
+            return None
+
+        data = response.json()
+        if "error" in data:
+            print(f"API error: {data['error']}")
+            return None
+        return data
